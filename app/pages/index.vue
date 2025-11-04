5ea6df2b0485ceb66bf3f6bf726515e394df143e
josephfoster05 <josephfoster2005@gmail.com>
2025-11-04 22:39:21 +0000

    brought over index from old project, images need sizing properly

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,41 @@
+
+
+
 <template>
-  <div class="home-container">
-    <h1>Home</h1>
-    <p>This is the home page. Use the navigation to go to the dashboard.</p>
-  </div>
+  <section id="title">
+      <h1>Sustain Wear</h1>
+    <img src="/images/LaundryStack.jpg" alt="An Image of Fresh Clothes Stacked Upon Each Other">
+  </section>
+
+
+  <section id="mainIntroduction">
+      <div class="infoBox">
+          <h3>What?</h3>
+          <p>At SustainWear we are Commited to Helping Reduce CO2 Emissions Caused by Clothes Waste. We aim to Provide a Friendly way to Donate Unwanted Clothes.</p>
+      </div>
+      <div id="seperator"><br></div>
+      <div class="infoBox">
+          <h3>Why?</h3>
+          <p>Pollution Has Become an Even More Prevalent Problem in the Modern Day. It's up to People Like us to Make a Change.</p>
+      </div>
+  </section>
+
+
+  <section id="variableStatistic">
+    <img src="/images/PollutionLandscape.jpg" alt="An Image of a foggy woodland with a large chute of polluted smoke in the background.">
+      <div id="infoBox">
+          <h3>2025</h3>
+          <p>This Year Alone we Have Reduced Over 750 Tonnes of CO2 Emissions That Would Have been Caused by Unwanted Clothing!</p>
+          <br>
+          <p>Consider donating to help contribute to our cause.</p>
+      </div>
+  </section>
+
+  <section id="variableStatistic">
+    <img src="/images/ClothesonHangers.jpg" alt="An Image of blazers hung by hangers with a soft, golden glow on the background.">
+
+  </section>
+
+  <hr class="divider">
+
 </template>
