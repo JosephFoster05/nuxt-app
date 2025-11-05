e981442fc2860f996e4f8e783fac572f9947e8a0
josephfoster05 <josephfoster2005@gmail.com>
2025-11-05 16:23:57 +0000

    updated some about us info

--- app/pages/about-us.vue
+++ app/pages/about-us.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>This is about us</p>
+    <h1>About Us</h1>
+    <p>Welcome to SustainWear! We are dedicated to promoting sustainable fashion and making a positive impact on the environment.</p>
+    <p>Our mission is to reduce clothing waste and encourage eco-friendly practices in the fashion industry. Join us in our journey towards a greener future!</p>
+    <p>Together, we can make a difference!</p>
+
+    <div class="image-container">
+        <img src="/images/LaundryStack.jpg" alt="Sustainable Fashion">
+        <section class="caption">Embracing Sustainable Fashion for a Better Tomorrow</section>
+    </div>
+
+    <nuxt-link to="/">Back to Home</nuxt-link>
+
 </template>
