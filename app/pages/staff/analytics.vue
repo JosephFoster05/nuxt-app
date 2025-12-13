bd8c3bbce6694d12ef2fb1b7aa01f4196575d9e3
ItsJustJoshua <darkfoxjnr123@gmail.com>
2025-12-13 01:03:41 +0000

    made analytics file and added basic things needed

---
+++ app/pages/staff/analytics.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, computed, ref } from "vue";
+import useAuth from "../../../composables/useAuth";
+import { useDonations } from "../../../composables/useDonations";
+
+const { user, fetchUserData, logout, error: authError } = useAuth();
+const {
+  donations,
+  fetchDonations,
+  loading: donationsLoading,
+  error: donationsError,
+  updateDonationStatus,
+} = useDonations();
+
+</script>
+
+<template>
+
+    <h1>analyics</h1>
+
+</template>
